04301329b34185582cd472dfb52331d9a069bda3
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-14 17:52:27 +0300

    Create Classes that is App,Squads and Heroes with their tests classes

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import model.Heroes;
+import model.Squads;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+}
