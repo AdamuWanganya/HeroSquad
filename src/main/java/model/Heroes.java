583cb264485b394273c17b65b4ee1a3291abe002
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-14 23:38:54 +0300

    Declare and create variables to be used in Heroes main method

--- src/main/java/model/Heroes.java
+++ src/main/java/model/Heroes.java
@@ -2,6 +2,14 @@ package model;
 
 public class Heroes {
 
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private static ArrayList<Heroes> instances = new ArrayList<>();
+    private static int id;
+    private boolean squadMember;
+
     public Heroes(String adamu, int i, String first, String fighter) {
     }
 }
