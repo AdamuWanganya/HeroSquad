bf6ac353ad9acd86af08cbacd3338d05889f3fb5
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-14 22:47:09 +0300

    Create and Declare variables in Squads class

--- src/main/java/model/Squads.java
+++ src/main/java/model/Squads.java
@@ -3,7 +3,18 @@ package model;
 import java.util.ArrayList;
 
 public class Squads {
+    private String name;
+    private String cause;
+    private int maxSize;
+    private ArrayList<Heroes> heroes;
+    private static ArrayList<Squads> squadInstances = new ArrayList<>();
+    private int id;
 
-    public Squads(String hackers) {
+    public Squads(String name, String cause, int size, ArrayList<Heroes> heroes){
+        this.name =name;
+        this.cause=cause;
+        this.maxSize=size;
+        this.heroes=heroes;
+        squadInstances.add(this);
+        this.id = squadInstances.size();
     }
-}
