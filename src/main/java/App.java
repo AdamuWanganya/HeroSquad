a151c7067ebc6d9a662985de56616760eaa0a6c5
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-14 18:05:41 +0300

    declare main App Class method and set staticFileLocation as /public

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,4 +10,8 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+    }
 }
