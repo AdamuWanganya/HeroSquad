04d960202acff2311f90bb26980aadccc9612ebc
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-14 23:27:24 +0300

    Create SquadsTest.java

--- src/main/java/model/Squads.java
+++ src/main/java/model/Squads.java
@@ -30,3 +30,32 @@ public class Squads {
     public int getSize() {
         return this.maxSize;
     }
+
+    public ArrayList<Heroes> getHeroes() {
+        return this.heroes;
+    }
+
+    public static ArrayList<Squads> getSquadInstances() {
+        return squadInstances;
+    }
+
+    public int setId(int id) {
+        return this.id=id;
+    }
+
+    public static void clearAllSquads() {
+        squadInstances.clear();
+    }
+
+    public static Squads findById(int id) {
+        try {
+            return squadInstances.get(id-1);
+        } catch (IndexOutOfBoundsException exception) {
+            return null;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+}
