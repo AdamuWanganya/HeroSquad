2d95ec589bcf432e2f076742ce64d5e67158c65a
AdamuWanganya <wanganyaadamu@gmail.com>
2021-12-14 23:35:44 +0300

    Instantiate HeroesTest object

--- src/main/java/model/Heroes.java
+++ src/main/java/model/Heroes.java
@@ -2,4 +2,6 @@ package model;
 
 public class Heroes {
 
+    public Heroes(String adamu, int i, String first, String fighter) {
+    }
 }
--- src/test/java/model/HeroesTest.java
+++ src/test/java/model/HeroesTest.java
@@ -1,7 +1,22 @@
 package model;
 
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class HeroesTest extends TestCase {
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+public class HeroesTest {
+
+
+    @Test
+    public void NewHeroesInstantiatesHeroObjectsCorrectly_true() {
+        Heroes hero = new Heroes("Adamu", 5, "first", "fighter");
+        assertTrue(hero instanceof Heroes);
+    }
 
 }
